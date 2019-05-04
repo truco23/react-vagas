723e65af0e15ca6710cf98d52284d051e6d91c68
Genilson <geprojetosbrasil@gmail.com>
2019-05-04 18:48:00 -0300

    Exibindo informações de edição e remoção somente se estiver logado

--- src/shared/components/card/CardComponent.js
+++ src/shared/components/card/CardComponent.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import LocalStorageService from '../../services/localStorage/LocalStorageService';
+
+const methodsLocalStorage = new LocalStorageService();
 
 class CardComponent extends Component {
+
+    state = {
+        logged: false
+    };
+
+    async componentDidMount() {
+
+        if(methodsLocalStorage.isLogged()) {
+            
+            await this.setState({ logged: true });
+            return;
+        }
+
+        this.setState({ logged: false });
+    }
     
     render() { 
         const { id, title, description } = this.props;
+        const { logged } = this.state;
+
         return (  
-        <section className="card">
-            <header className="card-header bg-primary text-white text-center">
-                { title }
-            </header>
-            <div className="card-body">
-                <p className="card-text">{ description }</p>
-                <Link to={ `${ id }` } className="btn btn-info col-6">Editar</Link>
-                <button className="btn btn-danger col-6">Remover</button>
-            </div>
-        </section>
+            <section className="card">
+                <header className="card-header bg-primary text-white text-center">
+                    { title }
+                </header>
+                <div className="card-body">
+                    <p className="card-text">{ description }</p>
+                    {
+                        logged
+                        ? <div>
+                            <Link to={ `${ id }` } className="btn btn-info col-6">Editar</Link>
+                            <button className="btn btn-danger col-6">Remover</button>
+                        </div>
+                        : ''
+                    }
+                
+                </div>
+            </section>
         );
     }
 }
